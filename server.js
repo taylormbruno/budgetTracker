39ae140fbbe8e80d517040111397f610d2634b86
Taylor Bruno <taylorbruno895@gmail.com>
2020-02-22 15:25:58 -0500

    init commit

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const compression = require('compression');
 
 const PORT = 3000;
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect('mongodb://localhost/budget', {
   useNewUrlParser: true,
   useFindAndModify: false
 });
